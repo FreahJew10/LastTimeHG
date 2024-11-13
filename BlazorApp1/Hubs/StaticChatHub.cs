a59cda059b92347c8edcd7f2428b688fb79c78b4
liran tal <lirant108@gmail.com>
2024-11-14 00:51:13 +0200

    change on login,talkwith and add static chathub

--- BlazorApp1/Hubs/StaticChatHub.cs
+++ BlazorApp1/Hubs/StaticChatHub.cs
@@ -1,4 +1,6 @@
-﻿namespace BlazorApp1.Hubs
+﻿using System.Collections.Generic;
+
+namespace BlazorApp1.Hubs
 {
     public static class StaticChatHub
     {
@@ -35,5 +37,21 @@
            return list;
             
         }
+        public static async Task< bool> IfKeyHasVal(string key) //בודק אם עבור מפתח מסויים שיש בספרייה יש גם ערך (נעשה בגלל בעיות שילוב של פעולות שפועלות בפתיחת הדף)
+                                           // onafterrender&&OnInitializedAsync
+        {
+
+            foreach (KeyValuePair<string, string> entry in conids)
+            {
+                if (entry.Key == key)
+                {
+                    if(string.IsNullOrEmpty(entry.Value))
+                        return true;
+                }
+
+            }
+            return false;
+
+        } 
     }
 }
