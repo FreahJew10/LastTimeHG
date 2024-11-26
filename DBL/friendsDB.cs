553037d323c33fe82e4e1ae8cf4ac60213a8b48c
liran tal <lirant108@gmail.com>
2024-11-26 21:04:36 +0200

    1.base design for chat 2.you can chat only if your friend is incuding you as his friend 2.1 added func to provide this option +change the des of the friends lidt in the usermenagement

--- DBL/friendsDB.cs
+++ DBL/friendsDB.cs
@@ -9,14 +9,65 @@ namespace DBL
 {
     public class friendsDB : BaseDB<friends>
     {
-        protected override Task<List<friends>> CreateListModelAsync(List<object[]> rows)
-        {
-            throw new NotImplementedException();
+
+            
+            }
+
+            return we_are_both_friends;
         }
 
-        protected override Task<friends> CreateModelAsync(object[] row)
+        private async Task<Person> GiveMyFriend(int myId,int friendid)//פעולה שמביאה חבר שאני גם חבר שלו
         {
-            throw new NotImplementedException();
+            if(await AreWeBothFriends(myId,friendid)==1)
+            {
+                PersonStudentDB studentDB = new PersonStudentDB();
+                return await studentDB.SelectByPkAsync(friendid);
+            }
+            return null;
+        }
+        private async Task<int> AreWeBothFriends(int mystudentid,int friendid)//בודקת האם שנינו חברים אחד של השני
+        {
+            if(await DoesRelationshipExist(mystudentid, friendid)&& await DoesRelationshipExist(friendid,mystudentid))
+            {
+                return 1;
+            }
+            return 0;
+        }
+        private async Task<bool> DoesRelationshipExist(int mystudentid, int friendid)
+        {
+            string sql = @$"Select
+            mylastyear.friends.studentid,
+            mylastyear.friends.studentfriendId
+            From
+            mylastyear.friends
+            Where
+            mylastyear.friends.studentid = @mystudentid And
+            mylastyear.friends.studentfriendId = @friendid";
+            Dictionary<string,object> data = new Dictionary<string,object>();
+            List<friends> friends = await SelectAllAsync(sql, data);
+            if (friends.Count == 1)
+                return  true;
+            else
+                return false;
+        }
+        protected  override async Task<List<friends>> CreateListModelAsync(List<object[]> rows)
+        {
+            List<friends> friends = new List<friends>();
+            foreach (object[] row in rows)
+        {
+                friends student = await CreateModelAsync(row);
+                friends.Add(student);
+
+            }
+            return friends;
+        }
+
+        protected async override Task<friends> CreateModelAsync(object[] row)
+        {
+            friends myfriend = new friends();
+            myfriend.studentid = int.Parse(row[0].ToString());
+            myfriend.studentfriendId = int.Parse(row[1].ToString());
+            return myfriend;
         }
 
         protected override List<string> GetPrimaryKeyName()
