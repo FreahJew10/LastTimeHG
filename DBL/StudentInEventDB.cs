16f4750b366d5e1847ffc920a5283b6a4b0cf152
Twelve <Twelve@DESKTOP-C0RGCSO>
2024-12-04 09:34:56 +0200

    1. add studentinevent &studentineventDB 2.i think priv bug was fixed

---
+++ DBL/StudentInEventDB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+namespace DBL
+{
+    public class StudentInEventDB : BaseDB<StudentInEvent>
+    {
+        protected override async Task<List<StudentInEvent>> CreateListModelAsync(List<object[]> rows)
+        {
+            List<StudentInEvent> studentsInevent = new List<StudentInEvent>();
+            foreach (object[] row in rows)
+            {
+                StudentInEvent studentinevent = await CreateModelAsync(row);
+                studentsInevent.Add(studentinevent);
+
+            }
+            return studentsInevent;
+        }
+
+        protected override async Task<StudentInEvent> CreateModelAsync(object[] row)
+        {
+            StudentInEvent studentInEvent=new StudentInEvent();
+            studentInEvent.studentid = int.Parse(row[0].ToString());
+            studentInEvent.eventid = int.Parse(row[1].ToString());
+            return studentInEvent;
+        }
+
+        protected override List<string> GetPrimaryKeyName()
+        {
+            List<string> list = new List<string>() { "studenid", "eventid"};
+            return list;
+        }
+
+        protected override string GetTableName()
+        {
+            return "studentinevent";
+        }
+    }
+}
---
+++ Models/StudentInEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class StudentInEvent
+    {
+        public StudentInEvent() { }
+        public StudentInEvent(int studentid, int eventid)
+        {
+            this.studentid = studentid;
+            this.eventid = eventid;
+        }
+    
+        public int studentid { get; set; }
+        public int eventid { get; set; }
+
+    }
+}
