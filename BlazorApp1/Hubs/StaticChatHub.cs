3825c6ac488c42b472cc586ab3afb668097c0968
Twelve <Twelve@DESKTOP-C0RGCSO>
2024-12-19 11:01:46 +0200

    1.fixed bugs and add changes

--- BlazorApp1/Hubs/StaticChatHub.cs
+++ BlazorApp1/Hubs/StaticChatHub.cs
@@ -7,19 +7,31 @@ namespace BlazorApp1.Hubs
         public static Dictionary<string, string> conids { get; set; } = new Dictionary<string, string>();
         public static Dictionary<string, List<string>> multyconidFORnotifications { get; set; } = new Dictionary<string, List<string>>();
 
-        public static async Task<bool> IsThisSpesificConTO_Email_ExistIn_multyconidFORnotifications(string email, string conid)// תחת האמייל של היוזרmultyconidFORnotifications האם נמצא כבר חיבור לדף ב
+        public static async Task<bool> IsThisSpecificEmailExistIn_multyconidFORnotifications(string email)
         {
-            if ( multyconidFORnotifications.ContainsKey(email))
+            if (multyconidFORnotifications.ContainsKey(email))
             {
-                foreach (string conids in multyconidFORnotifications[email])
-                {
-                    if (conids == conid)
-                    {
-                        return true;
-                    }
-                }
+               return true;
             }
             return false;
+        }
+
+        public static async Task<bool> IsThisSpesificConTO_Email_ExistIn_multyconidFORnotifications(string email, string conid)// תחת האמייל של היוזרmultyconidFORnotifications האם נמצא כבר חיבור לדף ב
+        {
+           
+                if (multyconidFORnotifications.ContainsKey(email))
+                {
+                    foreach (string conids in multyconidFORnotifications[email])
+                    {
+                        if (conids == conid)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            
+          
         }
 
         public static async Task<List<string>> GiveMyFriendConFORnotifications(string email)// multyconidFORnotifications עבור 
