dbf59385043c5e2b41617f9a609058ead9e9d10a
liran tal <lirant108@gmail.com>
2025-06-25 18:18:10 +0300

    password validation

--- TheApp/DataConstLink/APIs.cs
+++ TheApp/DataConstLink/APIs.cs
@@ -2,7 +2,7 @@
 {
     public class APIs
     {
-        private const string url = "https://j9x79xqp-7047.euw.devtunnels.ms/api/LastYear/";
+        private const string url = "https://nc77c5c1-7047.euw.devtunnels.ms/api/LastYear/";
         public const string LoginURL = url + "login";
         public const string RegisterURL = url+ "register";
         public const string calenderurl = url + "eventforcalender";
